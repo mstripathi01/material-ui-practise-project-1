ba8cf6e1520b798dcd0f7f2ffb5db1dd30104795
Mayank Tripathi <mayank.tripathi@kadellabs.com>
2023-05-10 11:20:13 +0530

    material-ui

---
+++ src/components/Feed.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { Post } from "./Post";
+
+export const Feed = () => {
+  return (
+    <Box flex={4} p={{ xs: 0, md: 2 }}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </Box>
+  );
+};
---
+++ src/components/Post.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Box, Card } from "@mui/material";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVert from "@mui/icons-material/MoreVert";
+
+export const Post = () => {
+  return (
+    <Box
+      bgcolor="lightgray"
+      flex={2}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+          title="Mayank Tripathi"
+          subheader="September 14, 2022"
+        />
+        <CardMedia
+          component="img"
+          height="20%"
+          image="https://images.unsplash.com/photo-1662608582554-eac8328869ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite sx={{ color: "red" }} />}
+            />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
